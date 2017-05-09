6db38d8fec27c2f74f470a99607903f1f7d1bcd9
CN Chen <schen08@gmail.com>
2017-05-09 13:15:56 -0400

    Update stream-dict.py

--- Code/Streaming/stream-dict.py
+++ Code/Streaming/stream-dict.py
@@ -2,11 +2,16 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 import Preprocess as pp
 import pandas as pd
+from datetime import datetime
+from operator import add
 
+# initialize SparkContext and StreamingContext
 sc = SparkContext(appName="SparkStreamingScoreDictionary") 
 ssc = StreamingContext(sc, 9)
+# set file stream input location
 filestream = ssc.textFileStream("hdfs:///user/cloudera/sentiment/input")
 
+# dictionary setting can either be 'Harvard' or 'Financial'
 DICTIONARY = 'Harvard'
 
 if DICTIONARY == 'Financial':
@@ -26,17 +31,21 @@ else:
     raise Exception('Error: Improper dictionary chosen.')
 
 
+# calculates a sentiment score between -1 and +1 based on counting words in positive and negative dictionary
 def calc_score(message):
+  # make all letters upper case and split into words
   words = message.upper().split()
+  # tabulate positive and negative word counts
   pos_count = sum([(word in fin_pos) for word in words])
   neg_count = sum([(word in fin_neg) for word in words])
+  # return score
   if pos_count + neg_count != 0:
     return 1.0*(pos_count - neg_count) / (pos_count + neg_count)
   else:
     return 0
 
 
-from datetime import datetime
+# takes raw input data and calculates and stores a score for the message sentiment
 def parseTweet(line):
   s = line.split(",")
   try:
@@ -46,8 +55,11 @@ def parseTweet(line):
         raise Exception('Wrong ticker')
       # clean up message
       clean_text = pp.lemmatize(pp.tag_and_remove(pp.remove_features(pp.remove_stops(s[4]))))
+      # calculate score
       score = calc_score(clean_text)
+      # parse message time
       tweet_time = datetime.strptime(s[3][:10]+' '+s[3][11:19], "%Y-%m-%d %H:%M:%S")
+      # dispaly time and score while streaming
       print(tweet_time)
       print(score)
       return [{"time": tweet_time, "tweetId": long(s[0]), "ticker": s[1],"Body": s[4], "sentiment_tag": s[5], "score": score}]
@@ -56,10 +68,11 @@ def parseTweet(line):
       return []
 
 orders = filestream.flatMap(parseTweet)
-from operator import add
+# calculate sum totals of all sentiment scores grouped by ticker
 sentimentPerTicker = orders.map(lambda o: (o['ticker'], o['score'])).reduceByKey(add)
+# save results to file
 sentimentPerTicker.repartition(1).saveAsTextFiles("hdfs:///user/cloudera/output/output", "txt")
 
+# start spark stream processing
 ssc.start()
 ssc.awaitTermination()
-# ssc.stop(False)
