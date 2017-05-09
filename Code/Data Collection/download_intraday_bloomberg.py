5e9c9cf8ce463b35973305303abe966f378eb64d
Mohan Patnam <mpatnam@gmail.com>
2017-05-09 12:10:04 -0400

    rename python files

---
+++ Code/Data Collection/download_intraday_bloomberg.py
@@ -0,0 +1,18 @@
+import pybbg
+from datetime import datetime
+from datetime import timedelta
+import pandas as pd
+
+export_path = 'H:/Course Docs/Big Data/Final Project/Data/IntradayPrices/'
+
+bbg = pybbg.Pybbg()
+bbg.service_refData()
+tickers = ['AAPL', 'FB', 'TSLA']
+fld_list = ['open', 'high', 'low', 'close', 'volume']
+
+end_date = datetime(datetime.today().year, datetime.today().month, datetime.today().day) + timedelta(days=-1)
+
+for ticker in tickers:
+    df = bbg.bdib(ticker+' US Equity', fld_list, datetime(2017, 3, 1), end_date, eventType='TRADE', interval=1)
+    df.index = [x - pd.to_timedelta(5, unit='h') for x in df.index]
+    df[fld_list].to_csv(export_path+'IntradayPrice.'+ticker+'.csv', cols=fld_list, index_label='date')
